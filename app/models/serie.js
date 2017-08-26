33a033b91290fc3ecd7b8723cc1af1f07405e00b
Unknown <matheus_connolyn@hotmail.com>
2017-08-26 15:44:25 -0300

    add series model

---
+++ app/models/serie.js
@@ -0,0 +1,87 @@
+{
+    technicalDetails: {
+        movieName: "",
+        releaseYear: "",
+        originalName: "",
+        durationaverage: "",
+        franchise: "",
+        parentalRating: "",
+        universe: "",
+        originalLanguage: "",
+        synonyms: [""],
+        availableLanguages: [""],
+        seasonsAmount: "",
+        episodesAmount: "", 
+        soundtrack: [{
+            name: "",
+            author: "",
+            lengthInSeconds: 0
+        }],
+        basedOnBook: "",
+        producers: [""]
+    },
+    about: {
+        sinposis: "",
+        keywords: [""],
+        genre: [""],
+        theme: [""]
+    },
+    cast: {
+        director: "",
+        creator: "",
+        production: "",
+        script: "",
+        list: [{
+            name: "",
+            sex: "",
+            character: "",
+            voiceActor: ""
+        }]
+    },
+    media: {
+        logo: "",
+        poster: "",
+        trailers: [""],
+        promoVideos: [""],
+        backdropImages: [""],
+        promoImages: [""],
+        screenshots: [""]
+    },
+    
+    seasons:[{
+        number: "",
+        year: "",
+        episodesAmount: "",
+        
+        media: {
+            poster: "",
+            trailers: [""],
+            promoVideos: [""],
+            backdropImages: [""],
+            promoImages: [""],
+            screenshots: [""]
+        
+        },
+        
+        episodes: [{
+            name: "",
+            number: "", 
+            director: "", 
+            creator: "",
+            sinposis: "", 
+            screenshots: [""]
+            
+        }] 
+        
+    }],
+    
+    sky: {
+        schedule: [{
+            day: "",
+            time: ""
+        }],
+        chanel: "",
+        cineSky: ""
+            
+    }
+}
