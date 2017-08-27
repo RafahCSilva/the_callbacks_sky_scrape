0753ccf7878591c9a24c940881b1b8cd0cc83555
Andre0991 <andre.peric.tavares@gmail.com>
2017-08-27 01:42:27 -0300

    alternative names

--- app/parsers/movies/WikipediaParser.js
+++ app/parsers/movies/WikipediaParser.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const cheerio = require('cheerio');
 
 var request = require('request');
+var rp = require('request-promise');
 
 var obj = JSON.parse(fs.readFileSync('../../guide.json', 'utf8'))
 console.log("oi")
@@ -9,9 +10,9 @@ console.log("oi")
 function extractGenrePt(body) {
     // TODO: Pode ser género
     const $ = cheerio.load(body);
-    return $('td:contains("Gênero")').next().children('a').map(function(i, node) {
+    return $('td:contains("Gênero")').next().children('a').map(function (i, node) {
         return $(node).text();
-});
+    });
 }
 
 function extractDuracaoPt(body) {
@@ -21,29 +22,31 @@ function extractDuracaoPt(body) {
 
 function extractIdiomaPt(body) {
     const $ = cheerio.load(body);
-    return $('td:contains("Idioma")').next().children('a').map(function(i, node) {
+    return $('td:contains("Idioma")').next().children('a').map(function (i, node) {
         return $(node).text().toLocaleLowerCase();
     })
 }
 
-function getAlternativeSpellings(title) {
+async function extractAlternativeNames(title) {
     let reqUrl = "http://en.wikipedia.org/w/api.php?action=query&format=json&list=backlinks&bltitle=" + title + "&blfilterredir=redirects&bllimit=5";
-    request.get({uri: reqUrl,
-                json: true,
-                encoding: 'utf-8',
-                function(error, response, body) {
-                    if (!error) {
-                    }
-                }
-            });
+    let body = await rp.get({
+        uri: reqUrl,
+        json: true,
+        encoding: 'utf-8'
+    });
+    const $ = cheerio.load(body);
+    return uniq(body.query.backlinks.map(function(node, i) {
+        return node.title.toLowerCase();
+    }));
 }
+
 
 
 function extractStarringPt(body) {
     const $ = cheerio.load(body);
-    return $('td:contains("Elenco")').next().children('a').map(function(i, node) {
+    return $('td:contains("Elenco")').next().children('a').map(function (i, node) {
         return $(node).text();
-});
+    });
 }
 
 function extractFormatPtOld(body) {
@@ -57,87 +60,106 @@ function extractFormatPtOld(body) {
     return returnFormat;
 }
 
-function getWikipediaCleanerHtml(title) {
+async function getWikipediaCleanerHtml(title) {
     let wikipediaEndpoint = 'https://pt.wikipedia.org/api/rest_v1/page/html/';
     let reqUrl = wikipediaEndpoint + title;
-    request.get({uri: reqUrl,
-                json: true,
-                encoding: 'utf-8'}, function (error, response, body) {
-                if(!error && response.statusCode == '200') {
-                    extractedSomeDataSucessfully = false;
-                    let objRet = {'result': {}};
+    let body = await rp.get({
+        uri: reqUrl,
+        json: true,
+        encoding: 'utf-8'
+    });
+    return body;
+}
 
-                    let genre = extractGenrePt(body);
-                    if (genre != null && genre.length != 0) {
-                        objRet.result.genre = genre.toArray();
-                        extractedSomeDataSucessfully = true;
-                    }
+async function getObject(body, title) {
+        extractedSomeDataSucessfully = false;
+        let objRet = { 'result': {} };
 
-                    let starring = extractStarringPt(body);
-                    if (starring != null && starring.length != 0) {
-                        objRet.result.starring = starring.toArray();
-                        extractedSomeDataSucessfully = true;
-                    }
+        let genre = extractGenrePt(body);
+        if (genre != null && genre.length != 0) {
+            objRet.result.genre = genre.toArray();
+            extractedSomeDataSucessfully = true;
+        }
 
-                    let format = extractFormatPtOld(body);
-                    if (format != null && format.length != 0) {
-                        objRet.result.format = format
-                        extractedSomeDataSucessfully = true;
-                    }
+        let starring = extractStarringPt(body);
+        if (starring != null && starring.length != 0) {
+            objRet.result.starring = starring.toArray();
+            extractedSomeDataSucessfully = true;
+        }
 
-                    let idioma = extractIdiomaPt(body);
-                    if (idioma != null && idioma.length != 0) {
-                        objRet.result.language = idioma.toArray();
-                        console.log(idioma.toArray().toString());
-                        extractedSomeDataSucessfully = true;
-                    }
+        let format = extractFormatPtOld(body);
+        if (format != null && format.length != 0) {
+            objRet.result.format = format
+            extractedSomeDataSucessfully = true;
+        }
 
-                    let duration = extractDuracaoPt(body);
-                    if (duration != null && duration.length != 0) {
-                        parsedDuration = parseDuration(duration);
-                        objRet.result.duration = duration;
-                        extractedSomeDataSucessfully = true;
-                    }
+        let idioma = extractIdiomaPt(body);
+        if (idioma != null && idioma.length != 0) {
+            objRet.result.language = idioma.toArray();
+            extractedSomeDataSucessfully = true;
+        }
 
-                    objRet.title = title;
-                    objRet.source = 'wikipedia';
-                    if (extractedSomeDataSucessfully) {
-                        pushToDB(objRet);
-                        console.log()
-                    }
-                }
-            })
+        let duration = extractDuracaoPt(body);
+        if (duration != null && duration.length != 0) {
+            parsedDuration = parseDuration(duration);
+            objRet.result.duration = duration;
+            extractedSomeDataSucessfully = true;
+        }
 
+        let alternativeNames = await extractAlternativeNames(title);
+        if(alternativeNames != null && alternativeNames.length != 0) {
+            objRet.result.alternativeNames = alternativeNames;
+            extractedSomeDataSucessfully = true;
+        }
+
+        objRet.title = title;
+        objRet.source = 'wikipedia';
+        if (extractedSomeDataSucessfully) {
+            pushToDB(objRet);
+            return objRet;
+        }
+        return null;
 }
 
 function pushToDB(objRet) {
     endPoint = "http://thecallbacks.ddns.net:8080/hack/data";
-    request.post({uri: endPoint,
-                  json: objRet}, function(err, resp) {
-    console.log(err, resp.body)
-}
-);
+    rp.post({
+        uri: endPoint,
+        json: objRet
+    });
 }
 
-function scrape(titulo) {
+async function getWikipediaMatches(titulo) {
     let reqUrl = "https://pt.wikipedia.org/w/api.php?action=opensearch&search=" + titulo + "&limit=5&namespace=0&format=json";
-    request.get({uri: reqUrl,
-                json: true,
-                encoding: 'utf-8'}, function (error, response, body) {
-                 if(!error) {
-                    hasFirstResult = body.length > 0 && body[1].length > 0;
-                    if(hasFirstResult) {
-                        let title = body[1][0];
-                        getWikipediaCleanerHtml(title);
-                        // console.log(title);
-                    }
-                }
-            })
+    let body = await rp.get({
+        uri: reqUrl,
+        json: true,
+        encoding: 'utf-8'
+    });
+    return body;
 }
 
-obj.hits.forEach(function(element) {
-    scrape(element._source.programTitle);
-}, this);
+async function scrape(titulo) {
+    console.log(titulo);
+    let matches = await getWikipediaMatches(titulo);
+    if (matches) {
+        hasFirstResult = matches.length > 0 && matches[1].length > 0;
+        if (hasFirstResult) {
+            let title = matches[1][0];
+            let cleanHtml = await getWikipediaCleanerHtml(title);
+            return await getObject(cleanHtml, title);
+        }
+    }
+}
+
+(async function execute () {
+        scrape("Game of Thrones");
+    obj.hits.forEach(function (element, i) {
+        if (i < 50) {
+            // scrape(element._source.programTitle);
+        }
+    }, this);
+})()
 
 function parseDuration(raw) {
     let myRegexp = /(\d+) [Mm]in/;
@@ -147,4 +169,8 @@ function parseDuration(raw) {
         retorno = match[1];
     }
     return retorno;
+}
+
+function uniq(a) {
+   return Array.from(new Set(a));
 }
