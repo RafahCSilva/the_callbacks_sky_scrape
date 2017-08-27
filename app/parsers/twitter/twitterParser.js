25b5b28721e9510efe6f35b588a6cd577362c909
RafahCSilva <rafahcsilva@gmail.com>
2017-08-27 01:07:36 -0300

    Use promises on twitter parser

--- app/parsers/twitter/twitterParser.js
+++ app/parsers/twitter/twitterParser.js
@@ -1,38 +1,36 @@
-function TwitterParser(termo, callback) {
-    var Twitter = require('twitter');
+function TwitterParser(pesquisa) {
+    return new Promise(function (resolve, reject) {
+        let Twitter = require('twitter');
 
-    var client = new Twitter({
-        consumer_key: '22y8KxH86Q5nnrI4ZyMNnw5wZ',
-        consumer_secret: 'LprIzM7A6d0HpbhiEa9i8CoIm10L3Za2dke5FuyBrQJ3JyuodP',
-        access_token_key: '901631610776416256-ibaS6oyYWTMQmkwh3KhQDTwRniAcj1l',
-        access_token_secret: '2IJdMwIN1GVE3Be844iMnDIt90KXvWrwHAQGkFIXJp6qp'
-    });
-
-    let query = {
-        q: termo,
-        count: 10,
-        lang: 'pt',
-        result_type: 'recent'
-    };
-
-    client.get('search/tweets', query, function (error, tweets, response) {
-        if (error) {
-            console.err('[ERROR] search/tweets error');
-            return;
-        }
-        let tweetas = [];
-
-        tweets.statuses.forEach(function (el, i) {
-            tweetas.push({
-                user: '@' + el.user.screen_name,
-                text: el.text
-            });
+        let twitter = new Twitter({
+            consumer_key: '22y8KxH86Q5nnrI4ZyMNnw5wZ',
+            consumer_secret: 'LprIzM7A6d0HpbhiEa9i8CoIm10L3Za2dke5FuyBrQJ3JyuodP',
+            access_token_key: '901631610776416256-ibaS6oyYWTMQmkwh3KhQDTwRniAcj1l',
+            access_token_secret: '2IJdMwIN1GVE3Be844iMnDIt90KXvWrwHAQGkFIXJp6qp'
         });
-        callback(tweetas);
+
+        let query = {
+            q: pesquisa,
+            count: 10,
+            lang: 'pt',
+            result_type: 'recent'
+        };
+
+        twitter.get('search/tweets', query, function (error, tweets, response) {
+            if (error) {
+                reject('[ERROR] search/tweets error: ', error);
+            } else {
+                let tweetas = [];
+                tweets.statuses.forEach(function (el, i) {
+                    tweetas.push({
+                        user: '@' + el.user.screen_name,
+                        text: el.text
+                    });
+                });
+                resolve(tweetas);
+            }
+        });
     });
 }
 
-
-TwitterParser('Game of Thrones', (tweets) => {
-    console.log(tweets);
-});
+module.exports = TwitterParser;
