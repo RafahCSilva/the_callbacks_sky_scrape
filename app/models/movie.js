bc3e7967bc9db59774212d86169e4bbfdd335579
Giulio Fernando Lopes Paiva Denardi <giulio.lpdenardi@gmail.com>
2017-08-27 01:27:30 +0000

    Adding distributors and more productors

--- app/models/movie.js
+++ app/models/movie.js
@@ -24,6 +24,7 @@ module.exports = {
         directors: [],
         creators: [],
         production: [],
+        distributors: [],
         script: "",
         list: [{
             name: "",
--- app/parsers/ImdbParser.js
+++ app/parsers/ImdbParser.js
@@ -36,59 +36,73 @@ var scrapePage = function (url, query) {
                 //get soundtrack
                 let soundtrackUrl = url + '/soundtrack';
                 request(soundtrackUrl, function (error, response, html) {
-                    const $2 = cheerio.load(html);
-                    $2('#soundtracks_content .soundTrack').each(function (i, elm) {
-                        let name = $2(elm).text().split('\n')[0];
-                        let writer = $2(elm).text().split('\n')[1].replace('Written by ', '');
-                        movie.technicalDetails.soundtrack.push({
-                            name: name.substr(0, name.length-1),
-                            writer: writer.substr(0, writer.length-1)
+                    if (!error && response.statusCode == 200) {
+                        const $2 = cheerio.load(html);
+                        $2('#soundtracks_content .soundTrack').each(function (i, elm) {
+                            let name = $2(elm).text().split('\n')[0];
+                            let writer = $2(elm).text().split('\n')[1].replace('Written by ', '');
+                            movie.technicalDetails.soundtrack.push({
+                                name: name.substr(0, name.length-1),
+                                writer: writer.substr(0, writer.length-1)
+                            });
                         });
-                    });
-                    
-                    movie.technicalDetails.movieName = $('.title_wrapper h1').text().replace('&nbsp;', '').trim();
-                    movie.technicalDetails.releaseYear = $('#titleYear > a').text();
-                    movie.technicalDetails.originalName = $('.title_wrapper h1').text().replace('&nbsp;', '').trim();
-                    movie.technicalDetails.duration = $('#title-overview-widget > div.vital > div.title_block > div > div.titleBar > div.title_wrapper > div.subtext > time').text();
-                    movie.technicalDetails.originalLanguage = $('#titleDetails > div:nth-child(5) > a').text();
-                    
-                    movie.about.sinposis = $('#titleStoryLine .inline.canwrap p').text();
-                    $('#titleStoryLine > div:nth-child(6) a').each(function (i, elm) {
-                        if ($(elm).text().indexOf('See All') < 0) {
-                            movie.about.keywords.push($(elm).text());
-                        }
-                    });
-                    
-                    $('.title_wrapper .subtext a').each(function (i, elm) {
-                        if ($(elm).attr('href').indexOf('genre') > 0) {
-                            movie.about.genre.push($(elm).find('span').text());
-                        }
-                    });
-                    
-                    $('#titleDetails > div:nth-child(17)').find('a span').each(function (i,j) {
-                        movie.technicalDetails.producers.push($(j).text());
-                    });
-                    
-                    $('#title-overview-widget > div.plot_summary_wrapper > div.plot_summary > div:nth-child(2) > span').find('a > span').each(function (i, elm) {
-                        movie.cast.directors = $(elm).text();
-                    });
-                    
-                    $('#title-overview-widget > div.plot_summary_wrapper > div.plot_summary > div:nth-child(3) > span').find('a > span').each(function (i, elm) {
-                        movie.cast.creators.push($(elm).text());
-                    });
-                    
-                    $('#titleDetails > div:nth-child(11) span[itemprop=creator] a span').each(function (i,j) {
-                        movie.cast.production.push($(j).text());
-                    });
-                    
-                    //TODO change for image base64
-                    movie.media.poster = $('#title-overview-widget > div.vital > div.slate_wrapper > div.poster > a > img').attr('src');
-                    
-                    return {
-                        title: query,
-                        source: 'IMDB',
-                        result : movie
+                        
+                        movie.technicalDetails.movieName = $('.title_wrapper h1').text().replace('&nbsp;', '').trim();
+                        movie.technicalDetails.releaseYear = $('#titleYear > a').text();
+                        movie.technicalDetails.originalName = $('.title_wrapper h1').text().replace('&nbsp;', '').trim();
+                        movie.technicalDetails.duration = $('#title-overview-widget > div.vital > div.title_block > div > div.titleBar > div.title_wrapper > div.subtext > time').text();
+                        movie.technicalDetails.originalLanguage = $('#titleDetails > div:nth-child(5) > a').text();
+                        
+                        movie.about.sinposis = $('#titleStoryLine .inline.canwrap p').text();
+                        $('#titleStoryLine > div:nth-child(6) a').each(function (i, elm) {
+                            if ($(elm).text().indexOf('See All') < 0) {
+                                movie.about.keywords.push($(elm).text());
+                            }
+                        });
+                        
+                        $('.title_wrapper .subtext a').each(function (i, elm) {
+                            if ($(elm).attr('href').indexOf('genre') > 0) {
+                                movie.about.genre.push($(elm).find('span').text());
+                            }
+                        });
+                        
+                        $('#titleDetails > div:nth-child(17)').find('a span').each(function (i,j) {
+                            movie.technicalDetails.producers.push($(j).text());
+                        });
+                        
+                        $('#title-overview-widget > div.plot_summary_wrapper > div.plot_summary > div:nth-child(2) > span').find('a > span').each(function (i, elm) {
+                            movie.cast.directors = $(elm).text();
+                        });
+                        
+                        $('#title-overview-widget > div.plot_summary_wrapper > div.plot_summary > div:nth-child(3) > span').find('a > span').each(function (i, elm) {
+                            movie.cast.creators.push($(elm).text());
+                        });
+                        
+                        //TODO change for image base64 and download
+                        movie.media.poster = $('#title-overview-widget > div.vital > div.slate_wrapper > div.poster > a > img').attr('src');
+                        
+                        request(url + '/companycredits', function (error, response, html) {
+                            if (!error && response.statusCode == 200) {
+                                const $cred = cheerio.load(html);
+                                
+                                $cred('h4#production').next().find('li a').each(function (i, elm) {
+                                    movie.cast.production.push($(elm).text());
+                                });
+                                
+                                $cred('h4#distributors').next().find('li').each(function (i, elm) {
+                                    movie.cast.distributors.push($(elm).text().replace(/\s+/g, ' ').trim());
+                                });
+                                
+                                console.log(movie);
+                            }
+                        });
                     }
+                    
+                    // return {
+                    //     title: query,
+                    //     source: 'IMDB',
+                    //     result : movie
+                    // }
                 });
             }
             
