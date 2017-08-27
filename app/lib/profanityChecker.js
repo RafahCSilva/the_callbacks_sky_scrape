ae11a973658bf6547ab04ea371b335497cc7b157
Andre0991 <andre.peric.tavares@gmail.com>
2017-08-27 03:50:59 -0300

    palavroes

---
+++ app/lib/profanityChecker.js
@@ -0,0 +1,15 @@
+var profanity = require('profanity-util');
+
+var fs = require('fs');
+var forbiddenList = fs.readFileSync('palavroes.txt').toString().split("\n");
+for(i in forbiddenList) {
+    console.log(forbiddenList[i]);
+}
+
+function hasProfanify(str) {
+    return profanity.purify(str.toLowerCase(), { forbiddenList: forbiddenList })[1].find(isNotEmpty) != null;
+}
+
+function isNotEmpty(str) {
+    return str != "";
+}
